9ef1d6f27855f40379ea7386eb374917864cc49e
Maksim Shilov <maksim.shilov@akvelon.com>
2021-12-06 14:22:50 +0300

    Python parser

--- get-new-tool-versions/parsers/verify-added-to-image/verify-python-parser.psm1
+++ get-new-tool-versions/parsers/verify-added-to-image/verify-python-parser.psm1
@@ -6,22 +6,20 @@ function Search-PythonVersionsNotOnImage {
         [string]$FilterArch
     )
     
-    $stableReleases = (Invoke-RestMethod $ReleasesUrl) |
-        Where-Object stable -eq $true |
-        ForEach-Object {$_.$FilterParameter.split(".")[0,1] -join"."} |
-        Select-Object -Unique
+    $stableReleases = (Invoke-RestMethod $ReleasesUrl) | Where-Object stable -eq $true
+    if ($ToolName -eq "Node") {
+        $stableReleaseVersions = $stableReleases | ForEach-Object {$_.$FilterParameter.split(".")[0] + ".0"} |
+            Select-Object -Unique
+    } else {
+        $stableReleaseVersions = $stableReleases | ForEach-Object {$_.$FilterParameter.split(".")[0,1] -join"."} |
+            Select-Object -Unique
+    }
     $toolsetUrl = "https://raw.githubusercontent.com/actions/virtual-environments/main/images/win/toolsets/toolset-2022.json"
     $latestVersion = ((Invoke-RestMethod $toolsetUrl).toolcache |
         Where-Object {$_.name -eq $ToolName -and $_.arch -eq $FilterArch}).versions |
         Select-Object -Last 1
     $latestMinorVesion = $latestVersion.Replace("*","0")
-    $versionsToAdd = $stableReleases | Where-Object {[version]$_ -gt [version]$latestMinorVesion}
+    $versionsToAdd = $stableReleaseVersions | Where-Object {[version]$_ -gt [version]$latestMinorVesion}
     
     return $versionsToAdd
 }
-
-$ToolName = 'Node'
-$ReleasesUrl = 'https://raw.githubusercontent.com/actions/node-versions/main/versions-manifest.json'
-$FilterParameter = 'version'
-$FilterArch = 'x64'
-
